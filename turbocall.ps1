c587637ff58f91d3c508783cabd5aa00795e3d70
Benoît Mayer <mayer.benoit@gmail.com>
2019-04-10 18:22:43 +0200

    Update turbocall.ps1

--- turbocall.ps1
+++ turbocall.ps1
@@ -1,39 +1,45 @@
+$startDate = Get-Date 
+$endDate = Get-Date 31/12/2030
+$iterations = 1000
+$spotPrice = 100
+$volatility = 0.0318
+$chinagov10y = 0.0332
+$dailyVolatility = $true
 
-$endDate = Get-Date 31/07/2019
-$iterations = 10
 
+
+$diff = ($endDate - $startDate).Days
+if ($dailyVolatility) {$mu = $annualVolatility} else {$mu = $annualVolatility / [math]::Sqrt(252)}
+$rf = [math]::Pow(1+$chinagov10y,(1/252)) - 1
 
 $tabl = @()
-$line = @()
-$date = Get-Date
-$diff = ($endDate - $date).Days
+$prlt = @()
+
+for ($j=1; $j -le $iterations; $j++) {
+$prit = New-Object PSObject
+$prit | Add-Member "Iteration" ("Iteration_" + $j) 
+$prit | Add-Member "Price" ($spotprice -as [Double])
+$prlt += $prit
+}
 
 for ($i=0; $i -le $diff; $i++) {
-        if($date.AddDays($i).DayOfWeek -notin "Saturday", "Sunday") {
-        $line += $date.AddDays($i)}
+        if($startDate.AddDays($i).DayOfWeek -notin "Saturday", "Sunday") {
 
         for ($j=1; $j -le $iterations; $j++) {
 
-        $("price_" + $j) = 100*(1+ (Get-RandomNormal)*0.2)
+            $Price = $prlt[($j-1)].Price
+            $Price = $Price*(1+$rf+(Get-RandomNormal)*$mu)
+            $prlt[($j-1)].Price = $Price
+            $iter = New-Object PSObject
+            $iter | Add-Member "Date" $startDate.AddDays($i).ToString("dd/MM/yyyy")
+            $iter | Add-Member "Iteration" ("Iteration_" + $j.ToString())
+            $iter | Add-Member "Price" $Price
+
+            $tabl += $iter
+        }
+
     }
-
-
-
-
-
-
-for ($i=1; $i -le $iterations; $i++) {
-
-$tabl = ($tabl | Select-Object *,("Iteration_" + $i))
-    
 }
-
-$tabl
-
-$Price_0 =100
-
-for ($i=1; $i -le $iterations; $i++) {New-Variable -Name "Price_$i" -Value $i}
-for ($i=1; $i -le $iterations; $i++) {Get-Variable -Name ("Price_" + ($i-1)) -ValueOnly}
 
 
 function Get-RandomNormal
@@ -46,4 +52,4 @@ function Get-RandomNormal
     return $RandomNormal
     }
 
-(Get-RandomNormal)*0.2
+$tabl | Export-Csv "C:\Users\mayer_b\Desktop\MonteCarlo\matrix.csv" -Delimiter ";"
